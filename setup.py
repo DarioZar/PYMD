d3dfd0e2447cd2b2adc468ad65c64210269eabe2
Dario <dariozarcone@gmail.com>
2021-01-27 05:59:05 +0100

    Disabled purepython compile option, add dev tools in .cfg, compile instructions for pair_corr module

--- setup.py
+++ setup.py
@@ -6,26 +6,41 @@ from setuptools import Extension, setup
 # Choose build option from toml file
 options = toml.load("pyproject.toml")["build-system"]
 PARALLEL = options["parallel"]
-PUREPYTHON = options["purepython"]
 GUI = options["gui"]
 
 extensions = []
 
-if not PUREPYTHON:
-    cythonfile = "src/cython/force_LJ"
-    cythonfile += "_par.pyx" if PARALLEL else ".pyx"
-    compileargs = ["-O3", "-ffast-math", "-march=native"]
-    compileargs += ["-fopenmp"] if PARALLEL else []
-    linkargs = ["-fopenmp"] if PARALLEL else []
-    extensions += [
-        Extension(
-            name="pymd.force_LJ",
-            sources=[cythonfile],
-            include_dirs=[get_include()],
-            extra_compile_args=compileargs,
-            extra_link_args=linkargs,
-        )
-    ]
+# Lennard-Jones Cython Extension
+cythonfile = "src/cython/force_LJ"
+cythonfile += "_par.pyx" if PARALLEL else ".pyx"
+compileargs = ["-O3", "-ffast-math", "-march=native"]
+compileargs += ["-fopenmp"] if PARALLEL else []
+linkargs = ["-fopenmp"] if PARALLEL else []
+extensions += [
+    Extension(
+        name="pymd.force_LJ",
+        sources=[cythonfile],
+        include_dirs=[get_include()],
+        extra_compile_args=compileargs,
+        extra_link_args=linkargs,
+    )
+]
+
+# Pair Correlation Cython Extension
+cythonfile = "src/cython/pair_corr"
+cythonfile += "_par.pyx" if PARALLEL else ".pyx"
+compileargs = ["-O3", "-lm", "-ffast-math", "-march=native"]
+compileargs += ["-fopenmp"] if PARALLEL else []
+linkargs = ["-fopenmp"] if PARALLEL else []
+extensions += [
+    Extension(
+        name="pymd.pair_corr",
+        sources=[cythonfile],
+        include_dirs=[get_include()],
+        extra_compile_args=compileargs,
+        extra_link_args=linkargs,
+    )
+]
 
 install_requires = ["numpy", "matplotlib"]
 if GUI:
